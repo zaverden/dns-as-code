2cb41c8ec77e4901144a87b59b180fa2b5b77aec
Denis Zavershinskiy <zaverden@gmail.com>
2022-05-17 17:47:11 +0700

    add t.exp

--- configs/experiments.js
+++ configs/experiments.js
@@ -1,6 +1,8 @@
 D_EXTEND(
     "zaverden.com",
     CNAME("exp", "hole.zaverden.com."),
-    CNAME("*.exp", "exp.zaverden.com.")
+    CNAME("*.exp", "exp.zaverden.com."),
+    CNAME("t.exp", "exp.zaverden.com."),
+    CNAME("*.t.exp", "exp.zaverden.com.")
   );
   
