8873a17f90d0c0958871b6cb879009e3193ec835
Denis Zavershinskiy <zaverden@gmail.com>
2022-05-17 18:12:15 +0700

    change t.exp to test.exp

--- configs/experiments.js
+++ configs/experiments.js
@@ -2,7 +2,7 @@ D_EXTEND(
     "zaverden.com",
     CNAME("exp", "hole.zaverden.com."),
     CNAME("*.exp", "exp.zaverden.com."),
-    CNAME("t.exp", "exp.zaverden.com."),
-    CNAME("*.t.exp", "exp.zaverden.com.")
+    CNAME("test.exp", "exp.zaverden.com."),
+    CNAME("*.test.exp", "exp.zaverden.com.")
   );
   
